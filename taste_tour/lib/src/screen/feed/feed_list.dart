4424fc3ac92c23e81584b91aeadd7122429e06a0
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-01-24 02:24:54 +0900

    :bug: fix: feed List Error

--- taste_tour/lib/src/screen/feed/feed_list.dart
+++ taste_tour/lib/src/screen/feed/feed_list.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:taste_tour/src/widget/feed_list_item.dart';
+
+import '../../widget/feed_list_item.dart';
 
 /**
  * box형태로 사용할 것 같아 FeedBox를 사용하는 게 더 좋을 듯요
  */
-@Deprecated("어짜피 전부 box형태로 사용할 것 같아서 필요 없어질 것 같네요.")  
-class FeedList extends StatefulWidget {
-  const FeedList({super.key});
+@Deprecated("어짜피 전부 box형태로 사용할 것 같아서 필요 없어질 것 같네요.")
+class FeedList extends StatelessWidget {
+  final List<dynamic> myBoards;
+  const FeedList(this.myBoards,{super.key});
 
-  @override
-  State<FeedList> createState() => _FeedListState();
-}
-
-class _FeedListState extends State<FeedList> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -32,4 +29,3 @@ class _FeedListState extends State<FeedList> {
     );
   }
 }
-
