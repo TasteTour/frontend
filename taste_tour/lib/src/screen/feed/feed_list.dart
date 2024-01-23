aa0c43f6154d8e6ae5215fc7e01eb04489e8c410
dahyeon-da <pop8261@naver.com>
2024-01-23 23:46:52 +0900

    🐛fix: feedList, feedListItem

--- taste_tour/lib/src/screen/feed/feed_list.dart
+++ taste_tour/lib/src/screen/feed/feed_list.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:taste_tour/src/widget/feed_list.dart';
+import 'package:taste_tour/src/widget/feed_list_item.dart';
 
-class FeedList extends StatelessWidget {
+class FeedList extends StatefulWidget {
   const FeedList({super.key});
 
+  @override
+  State<FeedList> createState() => _FeedListState();
+}
+
+class _FeedListState extends State<FeedList> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
