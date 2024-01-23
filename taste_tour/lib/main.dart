f9af878d5b589d44b6b94a159ebe29343610fd85
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-01-23 20:28:02 +0900

    :sparkles: feat: 토큰 여부 확인하여 로그인 생략 기능 구현

--- taste_tour/lib/main.dart
+++ taste_tour/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:taste_tour/src/app.dart';
+import 'package:taste_tour/src/controller/user_controller.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  await GetStorage.init();
+
+  //로그인 여부 (토큰 보유 여부)
+  final userController = Get.put(UserController());
+  bool isLogin = await userController.isLogin();
+
+  runApp(MyApp(isLogin));
 }
